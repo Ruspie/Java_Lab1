644acd0211665a28cab85468a6fbc2dab8136e71
Alex_Begun <begun4ik111@mail.ru>
2016-11-07 02:40:14 +0300

    Add fourth task - ArrayNumbers & ArrayNumbersTest

---
+++ src/com/Labs/ArrayOfPrime_Task_4/ArrayNumbers.java
@@ -0,0 +1,37 @@
+package com.Labs.ArrayOfPrime_Task_4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by asus on 07.11.2016.
+ */
+public class ArrayNumbers {
+
+    private List<Integer> numbers;
+
+    public ArrayNumbers(List<Integer> numbers){
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getArrayPrimeNumbers(){
+        List<Integer> listIndexPrimeNumber = new ArrayList<>();
+        int index = 0;
+        for(int number: numbers){
+            if (isPrime(number)){
+                listIndexPrimeNumber.add(index);
+            }
+            index++;
+        }
+        return listIndexPrimeNumber;
+    }
+
+    private boolean isPrime(int number){
+        int divider = 2;
+        while ((divider * divider <= number) && (number % divider != 0)){
+            divider++;
+        }
+        return divider * divider > number;
+    }
+}
---
+++ src/com/Labs/Tests/ArrayNumbersTest.java
@@ -0,0 +1,26 @@
+package com.Labs.Tests;
+
+import com.Labs.ArrayOfPrime_Task_4.ArrayNumbers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by asus on 07.11.2016.
+ */
+public class ArrayNumbersTest {
+    @Test
+    public void getArrayPrimeNumbers() throws Exception {
+        ArrayNumbers arrayNumbers = new ArrayNumbers(Arrays.asList(15, 26, 13, 2, 3, 5, 2, 4, 8, 2));
+        List<Integer> actualIndex = arrayNumbers.getArrayPrimeNumbers();
+
+        List<Integer> testIndex = Arrays.asList(2, 3, 4, 5, 6, 9);
+
+        Assert.assertEquals(actualIndex, testIndex);
+    }
+}
